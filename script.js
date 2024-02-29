d33f9093e879e07bc1fbec8bf8ffd2448a39fe87
swarochisa <swarochisa@gmail.com>
2024-02-29 15:57:13 -0800

    Add code and other final changes

--- script.js
+++ script.js
@@ -1,12 +1,154 @@
-const DEFAULT_NUM = 0;
-const lastScreen = document.getElementById(lastScreen);
-const currentScreen =document.getElementById(currentScreen);
-const clear = document.getAnimations(clear);
+var input_ls = '';
+var input_cs = '';
+var clickedOperator = '';
+var currentOperator ='';
+var clickedNumber = '';
+var operand1 = 0;
+var operand2 = 0;
+
+const lastScreen = document.getElementById('last_screen');
+const currentScreen =document.getElementById('current_screen');
+const btnClear = document.getElementById('btn_clear');
+const result = document.getElementById('equals'); 
 
 function clear()
 {
-    lastScreen.innerHTML ="";
-    currentScreen.innerHTML = "0";
+    lastScreen.textContent = '';
+    currentScreen.innerHTML = 0;
+    operand1 =0; 
+    operand2 =0; 
+    clickedOperator = '';
+    currentOperator = '';
+    input_cs='';
+    input_ls='';
+
+}
+ 
+btnClear.addEventListener('click', ()=> clear());
+
+window.onload = () => 
+{
+    lastScreen.innerHTML =`<p></p>`;
+    currentScreen.innerHTML = `<p>0</p>`;
+    operand1 = parseFloat(currentScreen.textContent);
 }
 
-clear.onclick = () => clear(); 
+
+var numbers = document.querySelectorAll(".btn.number");
+var operators = document.querySelectorAll(".btn.operator");
+
+result.addEventListener('click',() => 
+{
+    currentOperator = clickedOperator;
+    
+    if(!(input_ls))
+    {
+        
+        operand1 = parseFloat(input_cs);
+        input_ls = input_cs+"=";
+        updateLS(input_ls);  
+        input_cs ='';      
+        
+    } 
+
+    else if(input_cs)
+    {
+        operand2 = parseFloat(input_cs);
+        updateLS(operand1.toString()+currentOperator+operand2.toString()+'=');
+        operand1 = evaluate(operand1,currentOperator,operand2);        
+        updateCS(operand1);
+        input_cs ='';                        
+    }     
+    
+});
+
+
+var myNumber = function()
+{
+    clickedNumber = this.dataset.number;
+    if(!(clickedOperator))
+    {
+        input_cs += clickedNumber;
+        updateCS(input_cs);
+    }
+    else
+    {        
+        input_cs += clickedNumber;
+        updateCS(input_cs);
+    }  
+    
+}
+
+var myOperator = function()
+{
+        
+    currentOperator = clickedOperator;
+    clickedOperator = this.dataset.operator;
+
+    if(!(input_ls))
+    {
+        operand1 = parseFloat(input_cs);
+        input_ls +=input_cs+clickedOperator;
+        updateLS(input_ls);  
+        input_cs ='';      
+        
+    } 
+
+    else if(input_cs)
+    {
+        operand2 = parseFloat(input_cs); 
+        operand1 = evaluate(operand1,currentOperator,operand2); 
+        input_ls = operand1.toString()+clickedOperator  ;  
+        updateLS(input_ls);        
+        updateCS(operand1.toString());
+        input_cs ='';                        
+    }   
+
+    else if(currentOperator != clickedOperator)
+    {
+        input_ls = operand1.toString()+clickedOperator;  
+        updateLS(input_ls); 
+          
+    }
+
+}
+
+function updateCS(operand)
+{
+    currentScreen.textContent = operand;
+
+}
+
+function updateLS(input_ls)
+{
+    lastScreen.textContent = input_ls;    
+    
+}
+
+for(var i=0; i< numbers.length; i++)
+{
+    numbers[i].addEventListener('click',myNumber,false)
+}
+
+for( var j=0; j< operators.length;j++)
+{
+    operators[j].addEventListener('click', myOperator, false)
+}
+
+function evaluate(op1,a,op2)
+{
+    switch (a)
+    {
+        case '÷' : op1/=op2;
+        return op1.toPrecision(2);
+        case '-': op1 -= op2;
+        return op1.toPrecision(2);
+        case '+':  op1 +=op2;
+        return op1.toPrecision(2);
+        case '×': op1*=op2;
+        return op1.toPrecision(2);
+       
+    }
+
+}
+
